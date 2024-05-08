23f785e324b016beb35b79c8562a8a9ebb1b73ab
Tlalemasego <tlalemasego@gmail.com>
2024-05-08 15:20:50 +0200

    Feat: Add -Coded my solution on the index.js file. -Added comments on my read me file.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+
+
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
-
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+    let petInfo = /pet_[a-zA-Z0-9]/; // expression ensures input starts with "pet_" by one or more alphanumeric characters
+    let result;
+if (petInfo.test(input)) { // depending on the result, displays valid syntax or invalid syntax. 
+    result ="Valid Syntax";
+} else { 
+    result ="Invalid Syntax";
+}
+    document.getElementById('result').innerText = result;
 }
 
 
